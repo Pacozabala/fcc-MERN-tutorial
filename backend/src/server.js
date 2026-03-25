ef91f51bf3706de199c700df3e1c33f8bb72756d
Paco Antonio Zabala <paco.zabala@student.ateneo.edu>
2026-03-25 16:07:06 +0800

    feat: finished ch4 - db, schema, env

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,20 @@
 // const express = require("express") -> syntax when type: "commonjs"
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+
+connectDB();
 
 // Endpoint: combo of URL + HTTP method that lets client interact with specific resource
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-    console.log("Server started on PORT: 5001");
+app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
 });
+
