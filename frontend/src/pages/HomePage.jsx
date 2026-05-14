27d653f1131f8ad9208d832c331fbef0c2ac4e72
Paco Antonio Zabala <paco.zabala@student.ateneo.edu>
2026-05-14 16:33:50 +0800

    delete functionality

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar"
 import RateLimitedUI from "../components/RateLimitedUI";
 import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
 import api from "../lib/axios"
 
 import { useEffect, useState } from "react"
@@ -42,10 +43,12 @@ const HomePage = () => {
       <div className="max-w-7xl, mx-auto, p-4, m-6">
         {loading && <div className="text-center text-primary py-10">Loading notes. Please wait...</div>}
 
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map(note => (
-              <NoteCard key={note._id} note={note} />
+              <NoteCard key={note._id} note={note} setNotes={setNotes}/>
             ))}
           </div>
         )}
