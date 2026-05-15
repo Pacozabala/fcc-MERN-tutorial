6a24922a177f1e2e939c480a0437f4e7dde35a18
Paco Antonio Zabala <paco.zabala@student.ateneo.edu>
2026-05-15 17:30:46 +0800

    fix: some changes to server and axios for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,6 +12,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 // middleware is stuff performed before getting a response, used through app.use()
 // use cases: authentication, rate limiting
@@ -18,15 +20,25 @@ const PORT = process.env.PORT || 5001;
 // this middleware parses JSON bodies: req.body
 app.use(express.json())
 
-// middleware for getting rid of CORS error
-app.use(cors({
-    origin:"http://localhost:5173",
-}));
+if(process.env.NODE_ENV !== "production"){
+    // middleware for getting rid of CORS error
+    app.use(cors({
+        origin:"http://localhost:5173",
+    }));
+}
 
 app.use(rateLimiter);
 
 // Endpoint: combo of URL + HTTP method that lets client interact with specific resource
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 // connect to database first, then start listening on port
 connectDB().then(() => {
